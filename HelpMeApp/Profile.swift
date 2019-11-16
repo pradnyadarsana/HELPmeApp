d4624a2a3faeff26fb1c86c1540c3a271cb36b02
nikokevin29 <nikokevin29@gmail.com>
2019-11-16 15:10:33 +0700

    Improvement UI and button colorizing UI and change logo

---
+++ HelpMeApp/Profile.swift
@@ -0,0 +1,20 @@
+//
+//  Profile.swift
+//  
+//
+//  Created by Nicholas Kevin on 16/11/19.
+//
+
+import UIKit
+import FirebaseAuth
+class Profile: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ HelpMeApp/Report.swift
@@ -0,0 +1,24 @@
+//
+//  Report.swift
+//  HelpMeApp
+//
+//  Created by Nicholas Kevin on 16/11/19.
+//  Copyright © 2019 Smith. All rights reserved.
+//
+
+import UIKit
+
+class Report: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
